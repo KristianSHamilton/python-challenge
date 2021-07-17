7384b1dd067ebf5a1d6b0d6e64ec6f178bdf172c
Kristian Hamilton <kristian_ham123@hotmail.com>
2021-07-17 13:28:27 -0700

    msg

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,2 +1,15 @@
-csvPath = "./03_python_challenge/python-challenge"
-csv = csv.read(budget_data.csv)
+import os
+import csv
+
+csvPath = os.path.join( ".","budget_data.csv")
+
+with open(csvPath) as csvFile: 
+
+    csvReader = csv.reader(csvFile, delimiter = ",")
+
+    #print(csvreader)
+    
+    csvHeader = next(csvReader)
+    print(f"CSV Header: {csvHeader}")
+    for row in csvReader:
+        print(row)
