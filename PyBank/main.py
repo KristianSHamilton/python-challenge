4ff5956a8c926adff978c9dde931b46596de283e
Kristian Hamilton <kristian_ham123@hotmail.com>
2021-07-17 16:53:22 -0700

    progress

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,10 +6,12 @@ profitTotal = 0
 profitDelta = 0
 profitPrior = 0
 profitDeltaTotal = 0
+profitDeltaGreatest = 0
+profitDeltaLeast = 0
 
 csvPath = os.path.join( ".","budget_data.csv")
 
-
+#Read CSV from path
 with open(csvPath) as csvFile: 
 
     csvReader = csv.reader(csvFile, delimiter = ",")
@@ -20,21 +22,34 @@ with open(csvPath) as csvFile:
     firstRow = next(csvReader)
     profitDeltaTotal = int(firstRow[1])
     profitPrior = int(firstRow[1])
+    monthsTotal = 1
+    profitTotal = int(firstRow[1])
     
     for row in csvReader:
-
+        #increments month  variable for every row
         monthsTotal = monthsTotal + 1
+        #adds current  row's profit to the total profit variable
         profitTotal = profitTotal + int(row[1])
+        #calcs change in price by subtracting the last row's profit from the current
         profitDelta = int(row[1]) - profitPrior
-        print(profitDelta)
+        #keeps a running total of  the price changes by adding current row delta to total variable
         profitDeltaTotal = profitDeltaTotal + profitDelta
+        #now that profitPrior has  been  used in current loop, sets variable for next loop
         profitPrior = int(row[1])
+        #finds greatest Delta value
+        if profitDelta > profitDeltaGreatest:
+            profitDeltaGreatest = profitDelta
+        #finds smallest Delta value    
+        if profitDelta < profitDeltaLeast:
+            profitDeltaLeast = profitDelta
 
 
-#        print(row)
+avgChange = profitDeltaTotal/monthsTotal
 
-
-print(monthsTotal)
-print(profitTotal)
-print(profitDelta)
-print(profitDeltaTotal)
+print("Financial Analysis")
+print("-----------------------------")
+print(f"Total Months: {monthsTotal}")
+print(f"Total Profit: ${profitTotal}")
+print(f"Average Change: ${avgChange}")
+print(f"Greatest Increase in Profits: ${profitDeltaGreatest}")
+print(f"Greatest Decrease in Profits:  ${profitDeltaLeast}")
