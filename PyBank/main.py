0db573b53ebdaa7d831dea4d8ea6a41316ddc6a9
Kristian Hamilton <kristian_ham123@hotmail.com>
2021-07-17 16:21:30 -0700

    progress

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,15 +1,40 @@
 import os
 import csv
 
+monthsTotal = 0
+profitTotal = 0
+profitDelta = 0
+profitPrior = 0
+profitDeltaTotal = 0
+
 csvPath = os.path.join( ".","budget_data.csv")
+
 
 with open(csvPath) as csvFile: 
 
     csvReader = csv.reader(csvFile, delimiter = ",")
-
-    #print(csvreader)
     
     csvHeader = next(csvReader)
-    print(f"CSV Header: {csvHeader}")
+    
+    #skip headers in first row
+    firstRow = next(csvReader)
+    profitDeltaTotal = int(firstRow[1])
+    profitPrior = int(firstRow[1])
+    
     for row in csvReader:
-        print(row)
+
+        monthsTotal = monthsTotal + 1
+        profitTotal = profitTotal + int(row[1])
+        profitDelta = int(row[1]) - profitPrior
+        print(profitDelta)
+        profitDeltaTotal = profitDeltaTotal + profitDelta
+        profitPrior = int(row[1])
+
+
+#        print(row)
+
+
+print(monthsTotal)
+print(profitTotal)
+print(profitDelta)
+print(profitDeltaTotal)
